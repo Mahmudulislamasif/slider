d0e3ab984bc8e43287b8d427bcf957bd6933646b
Mahmudulislamasif <mahmudulislamasif2000@gmail.com>
2023-05-20 11:03:54 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('.slider1').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider2'
+    });
+
+    $('.slider2').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider1',
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true
+    });
+});
